52942bf8889e97389fa324e32312c698612d153c
coned <coned@coned-OptiPlex-7010.(none)>
2012-09-13 16:09:45 -0600

    Got the heiarchy of controllers working.

--- index.php
+++ index.php
@@ -6,19 +6,59 @@ require_once('.password');
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Dashboard to TeamworkPM Migration Utility</title>
-	<link href="css/teamworkpm._m.css?f=179176106" rel="stylesheet" />
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
-	<style type="text/css">
-	.tasktab {
-		display: none;
-	}
-	.disabled {
-		color: grey !important;
-	}
-	</style>
 	<script type="text/javascript">
+
+	/*
+	Controller for the entire app
+	*/
+	var MigrationUtilCntrl = function(){
+		this.userInfoCntrl; // The user info controller
+
+		// Initially hide views besides the first one 
+		$("#project_selection").hide();
+	};
+
+
+	/*
+	Wires subcontrollers up to their views
+	*/
+	MigrationUtilCntrl.prototype.wireSubControllers = function(){
+		this.userInfoCntrl = new UserInfoCntrl(this); // this refers to the controller
+	}
+
+	MigrationUtilCntrl.prototype.goToStep = function(stepId)
+	{
+		if(stepId == 2)
+			$("#project_selection").slideToggle();
+	};
+
+	/*
+	Controller for when the user enters their information.
+	*/
+	var UserInfoCntrl = function(migrationUtilCntrl){
+		 var migrationUtilCntrl = migrationUtilCntrl;
+
+		// connect the controller to the view			
+		$("#user_info_next_button").click(function(){	
+			migrationUtilCntrl.goToStep(2);
+		});
+	};
+
+	/*
+	Controller for when the user selects a course to migrate.
+	*/
+	var ProjectSelectionCntrl = function(migrationUtilCntrl){
+		// hide the initial view 	
+	};
+
+
 	//	<!--
 	$(document).ready(function() {
+		var migrationUtilCntrl = new MigrationUtilCntrl();
+		migrationUtilCntrl.wireSubControllers();
+
+
 		/*$("#requestType").change(function(e) {
 			$(".tasktab").css("display","none");
 			$("#"+e.target.options[e.target.selectedIndex].value).css("display","block");
@@ -39,82 +79,30 @@ require_once('.password');
 		});*/
 		
 		// Load all the courses that can be migrated from dashboard
-		$.post("Ajax/portal.php", {
+		/*$.post("Ajax/portal.php", {
 					method: "dashboard",
 					id: "38839",
 					action: "get_course_listing"},
 				function(data){
 					alert(data);
-				});
+				});*/
 	});
 	//	-->
 	</script>
 </head>
 <body>
-	<div id="LayoutMockup">
-		<div class="noSel" id="topNavBarNew"><div id="topRight">
-			<ul class="noSel" id="topRightNav">		
-				<li class="first" id="navquickadd"><a class="tipped disabled" href="javascript:;"><span>Quick Add</span></a></li>
-				<li id="navswitchproject"><a class="tipped disabled" href="javascript:;"><span>Switch Project</span></a></li>
-				<li id="navsettings"><a class="tipped disabled" href="javascript:;"><span>Settings</span></a></li>
-			</ul>
-		</div>
-		<div id="topLeftDashTabsNew">
-			<ul>
-				<li id="tl_dashboard"><a class="ql" href="https://byuis.teamworkpm.net/dashboard">Dashboard</a></li><li>|</li>
-				<li id="tl_everything"><a title="View tasks, milestones &amp; messages across all projects" href="https://byuis.teamworkpm.net/allitems">Everything</a></li><li>|</li>
-				<li id="tl_projects"><a class="ql" href="https://byuis.teamworkpm.net/projects">Projects</a></li><li>|</li>
-				<li id="tl_calendar"><a class="ql" href="https://byuis.teamworkpm.net/calendar">Calendar</a></li><li>|</li>
-				<li id="tl_statuses"><a class="ql" href="https://byuis.teamworkpm.net/statuses">Statuses</a></li><li>|</li>
-				<li id="tl_people"><a class="ql" href="https://byuis.teamworkpm.net/people">People</a></li>
-			</ul>
-		</div>
-		<div style="height:2.5em;">
-			&nbsp;
-		</div>
-		<div id="Tabs" class="sub">
-			<ul id="MainTabs">
-				<li class="first sel" id="tab_overview"><a class="ql" href="javascript:;">Specific User Tasks</a></li>
-				<li class="last" id="tab_tasks"><a class="ql" href="projects/76256-bio-041-200-/tasks">Special Dashboard Query</a></li>
-			</ul>
-		</div>
-	</div>
-	<div id="mainContent" class="section" style="margin-left: 1em;">
-		<div class="sectiontr"></div>
-		<div class="sectiontl"></div>
-		<div id="titleHolder">Title</div>
-		<div>
-			Hello world
-		</div>
-	</div>
-	<div id="taskExample">
-			<div id="task1138128" class="task hs" style="position: relative; "><div class="taskLHS"><img id="ti1138128" src="images/icons/checkBox.png" width="13" height="13" alt="" onclick="tw.CheckboxMarkTaskDone(1138128)" style="cursor:pointer;cursor:hand;margin-top:4px;"><div id="pOpt1138128" class="btnDrpDwnDiv huh"><a href="javascript:tw.ShowTaskDropDown(1138128)" class="btnDrpDwn"><span class="l"></span><span class="mid"></span><span class="l"></span></a></div><span class="tHl huh" id="tHl1138128"></span></div><div id="taskRHSH1138128" class="taskRHSH hs"><div id="taskRHS1138128" class="taskRHS"><a href="javascript:tw.ShowEditTask(1138128)" style="font-size:10px;text-decoration:none;" title="Ryan B., Maurianne D., Luke R., Heather B." class="taskBubble mine"><span class="l"></span><span class="r"></span><span class="n">You + 3 others</span></a><span class="estimate tipped" data-tipped-options="showDelay:1000" data-tipped="Estimated time"><a href="javascript:;" onclick="Lightbox.showBoxByAJAX( '?action=invoke.tasks.showLBTaskEstimates()&amp;taskId=1138128&amp;uid='+(new Date()).getTime(), 500, 250, true, null );">8&nbsp;hrs</a></span><a href="tasks/1138128" class="ql tipped" data-tipped-options="skin:'light',showDelay:1000,offset:{y:-5},hook:'topmiddle'" data-tipped="&lt;span style='font-size:9px'&gt;Created by Luke Rebarchik&lt;/span&gt;"><span id="taskName1138128" class="taskName">All courses that currently use BrightCove videos are broken - get response from Computer Ops on ETA for resolution</span></a> <span class="taskDue late" title="Thursday 23 August" reldate="230812">(13 days ago)</span><span class="time tipped" data-tipped="?action=invoke.tasks.getTip_showTimeOnTask()&amp;taskId=1138128&amp;projectId=73292&amp;uid=1346852792225" data-tipped-options="ajax:{cache:false},skin:'light',hideOn:'click-outside',showDelay:500" onclick="tw.GenericLoad('TimeReport.ShowLBLogTimeForTaskForm( 73292 , 1138128 )')"><img src="images/icons/timeItem.png" align="absmiddle" width="16" height="16" alt="" border="0"></span><a href="javascript:;" id="task1138128_commentCount" onclick="Tasks.OnClickBubble(1138128);" title="1 comment" class="taskComment1 tipped" data-tipped="?action=invoke.comments.getTip_objectLatestComment()&amp;projectId=73292&amp;objectType=task&amp;objectId=1138128" data-tipped-options="ajax:true,skin:'light',hideOn:'click-outside'">1</a></div></div></div>
-		</div>
-	</div>
-	<div><input type="text" id="api_key" placeholder="API Key"/></div>
-	<div>
-		<select id="requestType">
-			<option>-- Select the task you wish to perform --</option>
-			<option value="copyTasks">Migrate user tasks</option>
-			<option value="customQuery">Perform a custom query</option>
-		</select>
-	</div>
-	<div class="tasktab" id="copyTasks">
-		Hello world
-	</div>
-    
     <!--start of new interface -->
     <div id="user_info_view">
-    	<h2>Step 1: Please enter user info..</h2>
+    	<h2>Step 1: Please enter user info...</h2>
     	<table>
 		<tr><td>Dashboard username:</td><td><input type="text" /><td></tr>
 		<tr><td>Teamwork API Key:</td><td><input type="text" /><td></tr>
 	</table>
-	<button type="button">Next</button>
+	<button id="user_info_next_button" type="button">Next</button>
     </div>
 
-    <div id="db_project_selection">
-    	<h2>Step 1: Please select dashboard course to migrate</h2>
+    <div id="project_selection">
+    	<h2>Step 2: Please select dashboard course to migrate</h2>
        	<table id="dashboard_course_listing">
         </table>
     </div>
